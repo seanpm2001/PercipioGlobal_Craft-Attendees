7447be8241b4d2e95a545e4e6d5cac762ae031d4
Stefanie Gevaert <>
2022-01-06 17:23:24 +0100

    provided tailwind variants and min height

--- buildchain/tailwind.config.js
+++ buildchain/tailwind.config.js
@@ -20,8 +20,20 @@ module.exports = {
     },
     theme: {
         extend: {
+            minHeight: {
+                16: '4rem'
+            }
         }
     },
     corePlugins: {},
     plugins: [],
+    variants: {
+        extend: {
+            backgroundColor: ['peer'],
+            outline: ['peer'],
+            ring: ['peer'],
+            ringColor: ['peer'],
+            ringOpacity: ['peer']
+        }
+    }
 };
