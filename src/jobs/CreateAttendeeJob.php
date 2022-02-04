9971c29d0968995fb3c3c0d770c4017c9746fa40
Michael Thomas <michael@dadamotion.be>
2022-02-04 15:50:21 +0100

    fix: added missing orgUrn + sanitising

--- src/jobs/CreateAttendeeJob.php
+++ src/jobs/CreateAttendeeJob.php
@@ -15,9 +15,9 @@ class CreateAttendeeJob extends BaseJob
 
     public function execute($queue)
     {
-        $orgName = $this->config['orgName'] ?? '';
-        $name = $this->config['name'] ?? '';
-        $jobRole = $this->config['jobRole'] ?? '';
+        $orgName = htmlspecialchars($this->config['orgName'], ENT_QUOTES) ?? '';
+        $name = htmlspecialchars($this->config['name'], ENT_QUOTES) ?? '';
+        $jobRole = htmlspecialchars($this->config['jobRole'], ENT_QUOTES) ?? '';
         $days = $this->config['days'] ?? '';
         $eventId = $this->config['event'] ?? '';
         $file = $this->config['file'] ?? '';
