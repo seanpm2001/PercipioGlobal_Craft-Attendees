6abb6491e88185b2e70e6062d11f3a24b40cb5b1
Stefanie Gevaert <>
2023-01-30 09:40:54 +0100

    fix: updated bugfix on the deletions of the CSV files

--- src/console/controllers/CsvController.php
+++ src/console/controllers/CsvController.php
@@ -8,15 +8,7 @@ use percipiolondon\attendees\Attendees;
 
 class CsvController extends Controller
 {
-    public $days = 60;
-
-    public function options($actionID)
-    {
-        $options = parent::options($actionID);
-        $options[] = 'days';
-
-        return $options;
-    }
+    public int $maxAge = 5184000; //in seconds, default --> 60 days
 
     /**
      * Handles the deletion of CSV files older than the given period (dafault 60 days)
@@ -26,10 +18,11 @@ class CsvController extends Controller
     public function actionDelete()
     {
         $files = glob(App::parseEnv(Attendees::$plugin->getSettings()->csvStoragePath).DIRECTORY_SEPARATOR.'*');
-        $now = time();
+
+        $limit = time() - $this->maxAge;
 
         foreach ($files as $file) {
-            if (is_file($file) && ($now - filemtime($file) >= 60 * 60 * 24 * $this->days)) {
+            if (is_file($file) && (filemtime($file) < $limit)) {
                 unlink($file);
             }
         }
